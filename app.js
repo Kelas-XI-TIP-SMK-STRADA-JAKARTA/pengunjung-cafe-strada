1cced252b3f9b3ad095ae168b5f2d94d8dced0bc
limmi26 <kisamakiazuel@gmail.com>
2021-07-24 22:02:47 +0700

    perubahan 2

--- app.js
+++ app.js
@@ -1,31 +1,30 @@
 // Kerjakan kode JavaScript kalian di bawah ini
 
-var nama = prompt("Nama Pengunjung");
-var umur = prompt("Usia Pengunjung");
-var uang = prompt("Uang Pengunjung");
+var nama = ("Michelle Iskak");
+var umur = ("16");
+var uang = ("40000");
 var hargaJuice = 10000;
 var hargaKopi = 25000;
 
-if(nama !== ""){
-// Jika nama tidak kosong
-console.log();
+if (nama !== "") {
+    console.log([nama] + " Boleh Masuk");
+
+    if (umur > 13) {
+        console.log("hanya boleh memesan kopi");
+    }
+    if (uang > 25000) {
+        console.log("anda bisa pesan Kopi, sisa uang anda: " + [uang - hargaKopi]);
+    }
+    else if (umur < 13) {
+        console.log("hanya boleh memesan juice");
+    }
+    if (uang > 10000) {
+        console.log("anda bisa pesan Juice, sisa uang anda: " + [uang - hargaJuice]);
+    } else {
+        // Jika uang yang dimiliki tidak mencukupi, maka tampilkan di console
+        console.log("Uang tidak cukup. Kamu tidak bisa pesan");
+    }
+
 } else {
-// Jika nama dari si pengunjung kafe kosong, tampilkan di console
-console.log("Anda tidak boleh masuk!");
-}
-
-if(umur > 13){
-console.log("hanya boleh memesan kopi");
-} if(uang > 25000){
-console.log("anda bisa pesan minum, sisa uang anda: " + [uang - hargaKopi]);
-} 
-else if(umur < 13) {
-console.log("hanya boleh memesan juice");
-} if(uang > 10000){
-console.log("anda bisa pesan minum, sisa uang anda: " + [uang - hargaJuice]);
-}
-
-else {
-// Jika uang yang dimiliki tidak mencukupi, maka tampilkan di console
-console.log("Uang tidak cukup. Kamu tidak bisa pesan")
+    console.log("Anda tidak boleh masuk!")
 }
