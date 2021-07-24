613222b8e47503dd6fa908488814532d7d94898e
limmi26 <kisamakiazuel@gmail.com>
2021-07-24 22:13:04 +0700

    perubahan 3 (tambah komen)

--- app.js
+++ app.js
@@ -1,30 +1,33 @@
 // Kerjakan kode JavaScript kalian di bawah ini
 
-var nama = ("Michelle Iskak");
-var umur = ("16");
-var uang = ("40000");
-var hargaJuice = 10000;
-var hargaKopi = 25000;
+var nama = ("Michelle Iskak"); // Nama Pengunjung
+var umur = ("16"); // Umur Pengunjung
+var uang = ("40000"); // Uang Pengunjung
+var hargaJuice = 10000; // Harga Juice
+var hargaKopi = 25000; // Harga Kopi
 
 if (nama !== "") {
     console.log([nama] + " Boleh Masuk");
-
     if (umur > 13) {
+        // jika umur ditulis lebih dari 13 tahun maka kamu dapat memesan kopi
         console.log("hanya boleh memesan kopi");
     }
     if (uang > 25000) {
+        // uang anda akan dikurang dengan harga kopi
         console.log("anda bisa pesan Kopi, sisa uang anda: " + [uang - hargaKopi]);
     }
     else if (umur < 13) {
+        // jika umur ditulis kurang dari 13 tahun maka kamu dapat memesan juice
         console.log("hanya boleh memesan juice");
     }
     if (uang > 10000) {
+        // uang anda akan dikurang dengan harga juice
         console.log("anda bisa pesan Juice, sisa uang anda: " + [uang - hargaJuice]);
     } else {
-        // Jika uang yang dimiliki tidak mencukupi, maka tampilkan di console
+        // Jika uang yang dimiliki tidak mencukupi, maka kamu tidak bisa pesan minum
         console.log("Uang tidak cukup. Kamu tidak bisa pesan");
     }
-
 } else {
+    // jika variabel nama kosong maka anda tidak boleh masuk
     console.log("Anda tidak boleh masuk!")
 }
