852627c8f30756459368ff10d6739fac98b91ea7
myairin <irenewindagultom@gmail.com>
2021-07-24 11:33:26 +0700

    revisi kedua tugas pengunjung cafe strada

--- app.js
+++ app.js
@@ -1,21 +1,29 @@
 // Kerjakan kode JavaScript kalian di bawah ini
 
-var nama = "irene"
-namaKosong = "Anda tidak boleh masuk!";
-console.log("irene");
+//1. Jika nama dari si pengunjung kafe kosong, tampilkan di console "Anda tidak boleh masuk!" dan program selesai. Jika nama tidak kosong, lanjut ke step ke 2.
 
-var umurDewasa = 16;
-umurCukup = "coffe";
-var umur = 13
-umurKecil = "juice";
-console.log(umurDewasa + ' ' + umurCukup);
+var nama = "terisi irene";
+if (nama === "terisi irene") 
+    console.log('irene, lanjutkan administasi')
+else if (nama === "tidak terisi")
+    console.log("Anda tidak boleh masuk!")
 
-var pesanan = "coffe"
-var hargaJuice = 10000;
-var hargaCoffe = 25000;
-var uangPelanggan = 30000;
-var kurang = "Uang tidak cukup. Kamu tidak bisa pesan";
-var strukHarga = "Anda bisa pesan minum. Sisa uang anda: [5000]";
-console.log(pesanan + ' ' + hargaCoffe + ' ' + strukHarga);
+//2. Jika umur dari si pengunjung di bawah 13 tahun, maka ia hanya boleh memesan juice. Jika umur 13 tahun keatas, ia hanya boleh memesan kopi.
+
+let umur = 17;
+if (umur >= 16)
+console.log("irene, anda hanya boleh memesan coffe")
+else if (umur <= 13 )
+console.log("maaf, anda hanya boleh memesan juice")
 
 
+//Juice memiliki harga 10000, sedangkan kopi memiliki harga 25000.
+//Jika uang yang dimiliki tidak mencukupi, maka tampilkan di console "Uang tidak cukup. Kamu tidak bisa pesan.". 
+//Jika uang cukup, tampilkan "Anda bisa pesan minum. Sisa uang anda: [____]", dan gantilah [____] dengan sisa uang yang telah dikurangi oleh harga minuman.
+
+let uangPelanngan = 45000
+
+if (uangPelanngan >= 25000)
+console.log("irene, Sisa uang anda: [Rp.20000]")
+else if (uangPelanngan <= 25000)
+console.log( "Uang tidak cukup. Kamu tidak bisa pesan.")
