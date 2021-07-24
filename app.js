739ec8c8b536bf61fb8b4b9775e71758576680e9
myairin <irenewindagultom@gmail.com>
2021-07-24 09:56:12 +0700

    tugas pertama yaitu pengunjung cafe strada telah selesai

--- app.js
+++ app.js
@@ -1,1 +1,21 @@
 // Kerjakan kode JavaScript kalian di bawah ini
+
+var nama = "irene"
+namaKosong = "Anda tidak boleh masuk!";
+console.log("irene");
+
+var umurDewasa = 16;
+umurCukup = "coffe";
+var umur = 13
+umurKecil = "juice";
+console.log(umurDewasa + ' ' + umurCukup);
+
+var pesanan = "coffe"
+var hargaJuice = 10000;
+var hargaCoffe = 25000;
+var uangPelanggan = 3000;
+var kurang = "Uang tidak cukup. Kamu tidak bisa pesan";
+var strukHarga = "Anda bisa pesan minum. Sisa uang anda: [5000]";
+console.log(pesanan + ' ' + hargaCoffe + ' ' + strukHarga);
+
+
