01086f4ca0f265c59653692b49a477f20e2a51f9
sulthanarif <msarizky201@gmail.com>
2021-07-25 19:12:32 +0700

    perubahan 1

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // Kerjakan kode JavaScript kalian di bawah ini
+var nama= "";
+var umur= 16;
+var duit= 100000;
+var hargaJus= 10000;
+var hargaKopi= 25000;
+
+if (nama !== "") {
+    console.log([nama]+ " welcome to cafe");
+
+/*kalo umur pengunjung diatas 13 tahun bisa pesen kopi */
+ if (umur >= 13) {
+    console.log([nama]+ " kamu boleh pesen kopi");
+        if (duit >= 25000){
+            console.log("ini kopi mu, " + [nama] + " uang kembalimu " + [duit-hargaKopi]);
+            }
+        else {
+            console.log("uang mu tidak cukup, kamu tidak dapat memesannya.");
+            }    
+    }   
+/*kalo umur pengunjung dibawah 13 tahun cuma boleh pesen jus*/       
+if (umur >=0 && umur <= 13 ) {
+        console.log([nama]+" kamu cuma bisa pesen jus");
+        if (duit >= 10000){
+            console.log("ini jus mu, " + [nama] +" uang kembali mu "+ [duit-hargaJus])
+        }
+        
+        else {
+            console.log("uang mu tidak cukup, kamu tidak dapat memesannya.")
+        }
+    } 
+/*situasi jika pengunjungnya alien*/
+} else {
+    // kalo gak ada namanya gak boleh masuk
+    console.log("wait, this customer is ilegal.");
+}
