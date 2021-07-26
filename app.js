b934a81d30bca8c2c01a5035f844cfb99270be88
sulthanarif <msarizky201@gmail.com>
2021-07-26 08:47:42 +0700

    perapihan

--- app.js
+++ app.js
@@ -1,42 +1,42 @@
 // Kerjakan kode JavaScript kalian di bawah ini
-var nama= "sulthan";
-var umur= 16;
-var duit= 100000;
-var hargaJus= 10000;
-var hargaKopi= 25000;
+var nama = "sulthan";
+var umur = 16;
+var duit = 100000;
+var hargaJus = 10000;
+var hargaKopi = 25000;
 
 if (nama !== "") {
-    console.log([nama]+ " welcome to cafe");
+    console.log([nama] + " welcome to cafe");
 
-/*kalo umur pengunjung diatas 13 tahun bisa pesen kopi */
- if (umur >= 13) {
-    console.log([nama]+ " kamu boleh pesen kopi");
-        if (duit === 25000){
+    /*kalo umur pengunjung diatas 13 tahun bisa pesen kopi */
+    if (umur >= 13) {
+        console.log([nama] + " kamu boleh pesen kopi");
+        if (duit === 25000) {
             console.log("uang mu pas");
         }
-            else if (duit >= 25000){
-                console.log([nama] + " uang kembalimu " + [duit-hargaKopi]);
-                }
+         else if (duit >= 25000) {
+            console.log([nama] + " uang kembalimu " + [duit - hargaKopi]);
+        }
         else {
             console.log("uang mu tidak cukup, kamu tidak dapat memesannya.");
-            }     
-    } 
-           
-    /*kalo umur pengunjung dibawah 13 tahun cuma boleh pesen jus*/       
- if (umur >=0 && umur < 13 ) {
-        console.log([nama]+" kamu cuma bisa pesen jus");
-        if (duit === 10000){
+        }
+    }
+
+    /*kalo umur pengunjung dibawah 13 tahun cuma boleh pesen jus*/
+    if (umur >= 0 && umur < 13) {
+        console.log([nama] + " kamu cuma bisa pesen jus");
+        if (duit === 10000) {
             console.log("uang mu pas");
         }
-        else if (duit >= 10000){
-            console.log([nama] +" uang kembali mu "+ [duit-hargaJus])
+         else if (duit >= 10000) {
+            console.log([nama] + " uang kembali mu " + [duit - hargaJus])
         }
-        
+
         else {
             console.log("uang mu tidak cukup, kamu tidak dapat memesannya.")
         }
-    } 
-}   
+    }
+}
 
 /*situasi jika pengunjungnya alien*/
 else {
