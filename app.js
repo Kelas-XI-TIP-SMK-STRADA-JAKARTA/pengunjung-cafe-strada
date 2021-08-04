62edc78cf567f686475b41de25406bab1ea477db
rezaagust <reza.2020@smk-strada.sch.id>
2021-08-04 15:41:31 +0700

    tugas reza

--- app.js
+++ app.js
@@ -1,1 +1,19 @@
 // Kerjakan kode JavaScript kalian di bawah ini
+var nama = "reza";
+usia = 17;
+myMoney = 1000;
+var hargaKopi = 25000;
+var hargaJuice = 10000;
+var nama1 = (nama === 'Reza Agustian' ? ': Silakan masuk': 'Kosong nih isi dong :*');
+console.log ("===== Pengunjung Cafe Strada =====")
+if (nama === "") {
+    console.log (nama1)
+} else if (nama === "Reza Agustian"){
+    console.log (nama + '' + nama1);
+} else {
+    console.log(nama + nama1);
+}
+
+if (umur <=0) {
+    console.log ()
+} else if (umur >= 13) {
